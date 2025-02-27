48c239ca4d9363ad702a75b0bef764cc554aa20c
Maliq Adewale <maliqade@udel.edu>
2025-02-27 15:36:46 -0500

    commiting to save changes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,28 +5,61 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public float flapStrength;
     public LogicScript logic;
+    private Camera _mainCamera;
 
     public bool birdIsALive = true;
+     public AudioSource flapSFX; // Assign in Inspector
+    public AudioSource collisionSFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //once once when you start the game
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic1").GetComponent<LogicScript>();
+        _mainCamera = Camera.main;
+        myRigidbody.bodyType = RigidbodyType2D.Kinematic; // Disable physics initially
+        myRigidbody.linearVelocity= Vector2.zero;
     }
 
     // Update is called once per frame
-    void Update()
+     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true && birdIsALive){
-             myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        // Enable physics when the game starts
+        if (logic.gameIsActive && myRigidbody.bodyType != RigidbodyType2D.Dynamic)
+        {
+            myRigidbody.bodyType = RigidbodyType2D.Static;
+            myRigidbody.linearVelocity = Vector2.zero;
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         }
-       
 
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsALive && logic.gameIsActive)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+            flapSFX.PlayOneShot(flapSFX.clip); 
+        }
+
+        if (birdIsALive && logic.gameIsActive)
+        {
+            Vector2 screenMin = _mainCamera.ViewportToWorldPoint(Vector2.zero);
+            Vector2 screenMax = _mainCamera.ViewportToWorldPoint(Vector2.one);
+
+            if (transform.position.y < screenMin.y || transform.position.y > screenMax.y)
+            {
+                TriggerGameOver();
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        TriggerGameOver();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void TriggerGameOver()
     {
-        logic.gameOver();
-        birdIsALive = false;
+        if (birdIsALive)
+        {
+            collisionSFX.PlayOneShot(collisionSFX.clip);
+            logic.gameOver();
+            birdIsALive = false;
+        }
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,11 +7,45 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+     public AudioSource scoreSFX;
+     public Text highScoreText;
+     private int highScore;
+     public GameObject titleScreen;
+    public BirdScript bird;
+    public bool gameIsActive;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Load high score from PlayerPrefs
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateHighScoreUI();
+        titleScreen.SetActive(true);
+        gameOverScreen.SetActive(false);
+        gameIsActive = false; // Game starts inactive
+    }
+    
+    public void StartGame()
+    {
+        titleScreen.SetActive(false);
+        gameIsActive = true;
+        bird.myRigidbody.bodyType = RigidbodyType2D.Dynamic; // Enable physics
+        bird.myRigidbody.linearVelocity = Vector2.zero;
+        playerScore = 0;
+        scoreText.text = "0";
+    }
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd){
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
+        scoreSFX.PlayOneShot(scoreSFX.clip);
+
+        if (playerScore > highScore)
+        {
+            highScore = playerScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+            UpdateHighScoreUI();
+        }
     }
 
     public void restartGame(){
@@ -20,5 +54,18 @@ public class LogicScript : MonoBehaviour
 
     public void gameOver(){
         gameOverScreen.SetActive(true);
+        gameIsActive = false; // Stop the game
+    }
+
+    private void UpdateHighScoreUI()
+    {
+        if (highScoreText != null)
+        {
+            highScoreText.text =highScore.ToString();
+        }
+        else
+        {
+            Debug.LogError("High Score Text UI is NOT assigned in Inspector!");
+        }
     }
 }
